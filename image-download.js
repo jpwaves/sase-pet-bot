02f4a74356f41bf40d8bff26dd5eb13757319759
Justin Pong <jp.waves51@gmail.com>
2021-04-23 14:10:19 -0400

    Add missing export and documentation

--- image-download.js
+++ image-download.js
@@ -1,17 +1,26 @@
 import request from 'request';
 import { createWriteStream } from 'fs';
 
-const generateFilePath = (path, ext) => {
+/**
+ * Generates a unique filepath for where a file should be download to.
+ * @param {String} path : directories preceding the file name
+ * @param {String} ext : extension of the file
+ * @returns a String containing the download path for a file
+ */
+export const generateFilePath = (path, ext) => {
     return path + Date.now() + ext;
 }
 
-const url = "https://cdn.discordapp.com/attachments/834553741805617254/834643370549837845/Icon_-_Pushpop.jpg";
+/**
+ * Downloads the image from the url to the destination of the given file path
+ * @param {String} url : url to the image
+ * @param {String} filename : location to where image should be downloaded to
+ * @param {Function} callback : function that should be executed when image is done
+ * downloading
+ */
 export const download = (url, filename, callback) => {
     request(url, (err, res, body) => {
         // console.log(res.headers);
         request(url).pipe(createWriteStream(filename)).on('close', callback);
     });
 };
-download(url, generateFilePath('./downloads/', '.jpg'), () => {
-    console.log('download complete')}
-);
