8c95a31c51800125e8d0d6c979ec53b3ff42e82f
Justin Pong <jp.waves51@gmail.com>
2021-05-08 01:27:07 -0400

    Move to src directory

---
+++ src/dynamodb.js
@@ -0,0 +1,235 @@
+import 'dotenv/config.js';
+import { 
+    DynamoDBClient, 
+    GetItemCommand,
+    PutItemCommand, 
+    QueryCommand, 
+    ScanCommand,
+    UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+
+// constants for DynamoDB client and operations
+const region = process.env.S3_BUCKET_REGION;
+const table = process.env.DYNAMODB_TABLE_NAME;
+
+const client = new DynamoDBClient({ 
+    region: region
+});
+
+/**
+ * Uploads data for creating a Discord Embed Message to the DynamoDB
+ * @param {String} key Key associated with an image file in the S3 bucket
+ * @param {String} uploaderId Discord ID of uploader
+ * @param {String} petName Name of pet
+ * @param {String} description Message to go with pet image
+ */
+export const dynamoDBUpload = async (key, uploaderId, petName, description) => {
+    const params = {
+        Item: {
+            embedId: { S: key },
+            uploaderId: { S: uploaderId },
+            alreadyPostedInCycle: { BOOL: false },
+            petName: { S: petName },
+            description: { S: description }
+        },
+        TableName: table
+    };
+    
+    const command = new PutItemCommand(params);
+    try {
+        const data = await client.send(command);
+        console.log(data);
+        console.log('put successful');
+    } catch (error) {
+        console.log(error);
+        console.log(command);
+        console.log(params);
+    } finally {
+        console.log('put done');
+    }
+};
+
+//dynamoDBUpload();
+
+// NOTE: this method is currently unused
+/**
+ * Queries all the items in the DynamoDB whose alreadyPostedInCycle attribute is false
+ * @param {String} embedIdKey Target embedId of an item in the DynamoDB
+ * @param {String} uploaderIdKey Target uploaderId of an item in the DynamoDB
+ * @returns Array of all items that have not been sent yet in the current upload cycle
+ */
+export const dynamoDBQueryUnsentImages = async (embedIdKey, uploaderIdKey) => {
+    const params = {
+        KeyConditionExpression: 'embedId = :ei and uploaderId = :ui',
+        FilterExpression: 'alreadyPostedInCycle = :posted',
+        ExpressionAttributeValues: {
+          ':ei': { S: embedIdKey },
+          ':ui': { S: uploaderIdKey },
+          ':posted': { BOOL: false },
+        },
+        ProjectionExpression: 'embedId, uploaderId, alreadyPostedInCycle, petName, description',
+        TableName: 'sase-pet-bot-discord-embeds',
+    };
+    
+    const command = new QueryCommand(params);
+    try {
+        const results = await client.send(command);
+        results.Items.forEach((element, index, array) => {
+            console.log(index);
+            console.log(element.embedId.S);
+            console.log(array);
+          });
+        console.log('query successful');
+        console.log(results.Items);
+        return results.Items;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        console.log('query done');
+    }
+}
+
+// dynamoDBQueryUnsentImages('1619597379740.jpeg');
+
+/**
+ * Retrieves the item with matching embedId's and uploaderId's as the input
+ * @param {String} embedIdKey Target embedId to get from DynamoDB
+ * @param {String} uploaderIdKey Target uploaderId to get from DynamoDB
+ * @returns The item with the given embedId and uploaderId
+ */
+export const dynamoDBRetrieveItem = async (embedIdKey, uploaderIdKey) => {
+    const params = {
+        Key: {
+          embedId: { S: embedIdKey },
+          uploaderId: {S: uploaderIdKey }
+        },
+        ProjectionExpression: 'embedId, uploaderId, alreadyPostedInCycle, petName, description',
+        TableName: table
+    };
+
+    const command = new GetItemCommand(params);
+    try {
+        const result = await client.send(command);
+        console.log(result.Item);
+        console.log('retrieve successful');
+        return result.Item;
+    } catch(error) {
+        console.log(error);
+    } finally {
+        console.log('retrieve done');
+    }
+};
+
+// dynamoDBRetrieveItem('1619597379740.jpeg', '193375097254313984');
+
+/**
+ * Toggles the status of whether the particular item with the given embedId and uploaderId has been posted already or not
+ * @param {String} embedIdKey Target embedId
+ * @param {String} uploaderIdKey Target uploaderId
+ */
+export const togglePosted = async (embedIdKey, uploaderIdKey) => {
+    const item = await dynamoDBRetrieveItem(embedIdKey, uploaderIdKey);
+    const params = {
+        ExpressionAttributeValues: {
+            ':toggledState': { BOOL: !item.alreadyPostedInCycle.BOOL }
+        },
+        Key: {
+            embedId: { S: embedIdKey },
+            uploaderId: { S: uploaderIdKey },
+        },
+        TableName: table,
+        UpdateExpression: 'SET alreadyPostedInCycle = :toggledState'
+    };
+
+    const command = new UpdateItemCommand(params);
+    try { 
+        const data = await client.send(command);
+        console.log(data);
+        console.log('toggle successful');
+    } catch(error) {
+        console.log(error);
+    } finally {
+        console.log('toggle done');
+    }
+}
+
+// await dynamoDBRetrieveItem('1619597379740.jpeg', '193375097254313984');
+// await togglePosted('1619597379740.jpeg', '193375097254313984');
+// await dynamoDBRetrieveItem('1619597379740.jpeg', '193375097254313984');
+
+/**
+ * Gets all the items that have been posted already in the Discord text channel
+ * @returns Array of all the data in DynamoDB that have been posted already
+ */
+const dynamoDBGetAllSent = async () => {
+    const params = {
+        FilterExpression: 'alreadyPostedInCycle = :state',
+        ExpressionAttributeValues: {
+            ':state': { BOOL: true },
+        },
+        ProjectionExpression: 'embedId, uploaderId',
+        TableName: table,
+    }
+
+    const command = new ScanCommand(params);
+    try {
+        const data = await client.send(command);
+        console.log(data.Items);
+        console.log('get all sent successful');
+        return data.Items;
+    } catch(error) {
+        console(error);
+    } finally {
+        console.log('get all sent done');
+    }
+}
+
+// await getAllItems();
+
+/**
+ * Modifies all the data in DynamoDB so the alreadyPostedInCycle attribute is false
+ */
+export const resetData = async () => {
+    try {
+        const data = await dynamoDBGetAllSent();
+        data.forEach(async item => {
+            console.log(item);
+            await togglePosted(item.embedId.S, item.uploaderId.S);
+        });
+        console.log('number of items reset: ' + data.length);
+    } catch(error) {
+        console.log(error);
+    } finally {
+        console.log('reset done');
+    }
+};
+
+// await resetData();
+
+/**
+ * Gets all the items that haven't been posted yet in the current posting cycle
+ * @returns Array of all items that are marked as not having been posted yet in the current cycle
+ */
+export const dynamoDBGetAllUnsent = async () => {
+    const params = {
+        FilterExpression: 'alreadyPostedInCycle = :state',
+        ExpressionAttributeValues: {
+            ':state': { BOOL: false },
+        },
+        ProjectionExpression: 'embedId, uploaderId',
+        TableName: table,
+    }
+
+    const command = new ScanCommand(params);
+    try {
+        const data = await client.send(command);
+        console.log(data.Items);
+        console.log('get all unsent successful');
+        return data.Items;
+    } catch(error) {
+        console(error);
+    } finally {
+        console.log('get all unsent done');
+    }
+}
+
+// await dynamoDBGetAllUnsent();
---
+++ src/index.js
@@ -0,0 +1,158 @@
+import 'dotenv/config.js';
+import { 
+    Client,
+    MessageAttachment } from 'discord.js';
+import { 
+    clearDownloadFolder, 
+    download, 
+    generateFilePath } from './image-download.js';
+import { 
+    s3getImage,
+    s3upload } from './s3.js';
+import { 
+    dynamoDBGetAllUnsent,
+    dynamoDBRetrieveItem,
+    dynamoDBUpload, 
+    resetData,
+    togglePosted } from './dynamodb.js';
+import { 
+    RecurrenceRule, 
+    scheduleJob } from 'node-schedule';
+
+// construct Discord client for bot
+const client = new Client();
+
+// Upon startup, begin scheduled job for posting images
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    // standard rule for scheduled job
+    // const rule = new RecurrenceRule();
+	// rule.hour = 10;
+	// rule.minute = 0;
+
+    // rule for testing scheduled job
+    const rule = '*/30 * * * * *';
+
+    // scheduled posting
+	scheduleJob(rule, () => {
+		console.log('Ran postDailyPetEmbedMessage on scheduled time at ' + new Date() + '\n');
+		postDailyPetEmbedMessage();
+	});
+});
+
+/**
+ * Handles event where users try to upload an image that bot can post later on.
+ */
+client.on('message', async message => {
+    try {
+        if (message.content.startsWith('!upload') && message.channel.id == process.env.DISCORD_TARGET_TEXT_CHANNEL_ID) {
+            // redirecting user to private message !upload to begin upload process
+            console.log('Cannot upload in public text channel');
+            message.delete({ reason: 'Cannot upload in public text channel' });
+            const msg = 'To upload a new pet image, private message this bot !upload to start the upload process. When uploading an image file, if you want to include the pet name, and a message to go with the photo do the following formatting:';
+            message.author.send(msg);
+        } else if (message.content.startsWith('!upload') && message.channel.type == 'dm') {
+            if (message.attachments.size != 1) {
+                message.author.send('Missing image file/can only upload 1 image per upload operation');
+            } else {
+                const iter = message.attachments.values();
+                const messageFile = iter.next().value;
+    
+                const content = message.content.slice(7, message.content.length);
+                const args = content.split('|').map(arg => {
+                    return arg.trim();
+                });
+                console.log(args);
+                
+                const petName = args[0];
+                const desc = args[1];
+
+                try {
+                    // TODO: convert download to return a read stream that can be piped directly into s3upload
+                    // see https://stackoverflow.com/questions/14544911/fs-createreadstream-equivalent-for-remote-file-in-node
+                    // for a possible starting point on how to do this
+                    console.log('starting download');
+                    download(messageFile.url, generateFilePath('./downloads/'), async () => {
+                        console.log('download complete');
+
+                        console.log('starting s3 upload');
+                        const key = await s3upload('./downloads/', () => {
+                            console.log('upload to s3 bucket complete');
+                        });
+    
+                        console.log('starting dynamodb upload');
+                        dynamoDBUpload(key, message.author.id, petName, desc)
+                        .then(() => {
+                            console.log('upload to dynamodb complete');
+                        })
+                        .then(async () => {
+                            console.log('starting image removal to clean downloads folder');
+                            await clearDownloadFolder(key);
+                            console.log('clean up complete');
+                        })
+                        .then(async () => {
+                            await message.author.send('Upload Complete!');
+                            console.log('upload process complete');
+                        })
+                        .catch(error => {
+                            console.log('Error occurred during dynamodb upload process');
+                            console.log(error);
+                        });
+                    });
+
+                } catch(error) {
+                    console.log(error);
+                    message.author.send('Upload failed. Check logs for more details.');
+                }
+            }
+        }
+    } catch(error) {
+        console.log(error);
+    }
+});
+
+/**
+ * Gets data for a random, not-yet-posted Discord embed, creates the embed and posts it
+ * in the target text channel.
+ */
+const postDailyPetEmbedMessage = async () => {
+    // gets all items that haven't been sent yet in this posting cycle
+    const data = await dynamoDBGetAllUnsent();
+
+    // gets random item from data
+    const randIdx = Math.floor(Math.random() * data.length);
+    const randItem = data[randIdx];
+    
+    // gets embed data from random item
+    const embedData = await dynamoDBRetrieveItem(randItem.embedId.S, randItem.uploaderId.S);
+    // console.log(embedData);
+
+    // gets image associated with embedData
+    const filestream = await s3getImage(embedData.embedId.S, () => {
+        console.log('get image complete');
+    });
+    
+    // cycle resetting
+    if (data.length == 1) {
+        await resetData();
+    }
+    await togglePosted(embedData.embedId.S, embedData.uploaderId.S);
+
+    // creating Discord message embed and posting it to channel
+    const imageLink = `attachment://${embedData.embedId.S}`;
+    const imageFile = new MessageAttachment(filestream, embedData.embedId.S);
+    const msgEmbed = {
+        title: embedData.petName.S,
+        image: {
+            url: imageLink,
+        },
+        author: embedData.uploaderId.S,
+        description: embedData.description.S
+    };
+    //message.author.send({ files: [imageFile], embed: msgEmbed });
+    client.channels.cache.get(process.env.DISCORD_TARGET_TEXT_CHANNEL_ID).send({ files: [imageFile], embed: msgEmbed });
+}
+
+// Logs in bot
+client.login(process.env.DISCORD_BOT_TOKEN);
